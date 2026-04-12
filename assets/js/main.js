87664f9ffcb9de90f5ec5375125d8d2bd46613e0
m-ibrahim-x <muhammedibrahim2268@gmail.com>
2026-04-12 03:06:48 +0200

    initial commit - start E-commerce Project

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+function toggleSidebar() {
+    const sb = document.getElementById("sidebar");
+    const texts = document.querySelectorAll(".sidebar-text");
+
+    // main
+    const main = document.getElementById("mainContent");
+
+
+    const isOpen = sb.classList.contains("w-32");
+
+    if (isOpen) {
+        sb.classList.replace("w-32", "w-16");
+        main.classList.replace("mr-32", "mr-16");
+        texts.forEach(el => el.classList.add("hidden"))
+    } else {
+        sb.classList.replace("w-16", "w-32");
+        main.classList.replace("mr-16", "mr-32");
+        texts.forEach(el => el.classList.remove("hidden"))
+    }
+
+}
